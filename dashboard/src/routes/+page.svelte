1c75d0f98a633aeca96a4cfa125a29dfdac7dfea
Celeste Gambardella <cg7346@rit.edu>
2023-01-25 16:47:07 -0500

    new library components

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import {
+    Content,
+    Grid,
+    Row,
+    Column,
+    ImageLoader,
+  } from "carbon-components-svelte";
+  import "carbon-components-svelte/css/all.css";
+  import "../css/index.css";
+  import Navigation from "../components/Navigation.svelte";
+
+  let logo =
+    "https://www.womenofthehall.org/wp-content/themes/NWHoF/assets/imgs/logo-nwhof.png";
+</script>
+
+<main>
+  <header><Navigation /></header>
+  <Content>
+    <Grid>
+      <Row>
+        <ImageLoader style="height: 8rem; width: 20rem;" src={logo} />
+      </Row>
+    </Grid>
+  </Content>
+</main>
--- dashboard/src/routes/nominations/[id]/+page.svelte
+++ dashboard/src/routes/nominations/[id]/+page.svelte
@@ -1,19 +1,44 @@
-<script>
+<script lang="ts">
+  import "carbon-components-svelte/css/all.css";
+  import "../../../css/index.css";
+  import Navigation from "../../../components/Navigation.svelte";
+  import {
+    Content,
+    SideNavDivider,
+    Column,
+    Row,
+  } from "carbon-components-svelte";
   export let data;
-  export let {n} = data.props;
+  export let { n } = data.props;
 </script>
 
-<h1>{n.nominee.firstName} {n.nominee.lastName}</h1>
-<h3>Nominated by {n.nominator.firstName} {n.nominator.lastName} on {n.date}.</h3>
+<main>
+  <header><Navigation /></header>
+  <body>
+    <Content style="padding-left: $spacing-05">
+      <Column>
+        <h1>Nomination</h1>
 
-<p>Nomination Category: <b>{n.nominee.category}</b></p>
+        <h2>{n.nominee.firstName} {n.nominee.lastName}</h2>
 
-<p>This nominee is{n.nominee.deceased ? ' ' : ' not '}dead.</p>
+        <h3>
+          Nominated by {n.nominator.firstName}
+          {n.nominator.lastName} on {n.date}.
+        </h3>
 
+        <p>Nomination Category: <b>{n.nominee.category}</b></p>
 
-<form method="POST" action="?/createNomination">
-  <input type="text" name="title" placeholder="Title">
-  <input type="text" name="body" placeholder="Body">
-  <input type="number" name="userId" value="6">
-  <input type="submit" value="Create!">
-</form>
+        <p>
+          This nominee is{n.nominee.deceased ? " " : " not "}dead.
+        </p>
+
+        <form method="POST" action="?/createNomination">
+          <input type="text" name="title" placeholder="Title" />
+          <input type="text" name="body" placeholder="Body" />
+          <input type="number" name="userId" value="6" />
+          <input type="submit" value="Create!" />
+        </form>
+      </Column>
+    </Content>
+  </body>
+</main>
