834230d3194672485d1d3470649a4a88f3fd0172
Celeste Gambardella <cg7346@rit.edu>
2023-01-26 20:23:27 -0500

    updated home with dummy data
    TODO: this will be replaced once backend is setup

--- dashboard/src/components/dashHome/JudgeProcess.svelte
+++ dashboard/src/components/dashHome/JudgeProcess.svelte
@@ -1,5 +1,9 @@
 <script lang="js">
   import { Content, Grid, Row, Column, Tile } from "carbon-components-svelte";
+
+  let notOpenedCount = 50;
+  let inProgressCount = 10;
+  let completedCount = 36;
 </script>
 
 <Content>
@@ -8,14 +12,14 @@
     <Grid>
       <Row>
         <Column>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
+          <Row><h4>{notOpenedCount}</h4></Row>
+          <Row><h4>{inProgressCount}</h4></Row>
+          <Row><h4>{completedCount}</h4></Row>
         </Column>
         <Column>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
+          <Row><h5>Not Opened</h5></Row>
+          <Row><h5>In-Progress</h5></Row>
+          <Row><h5>Completed</h5></Row>
         </Column>
       </Row>
     </Grid>
--- dashboard/src/components/dashHome/NominationOverview.svelte
+++ dashboard/src/components/dashHome/NominationOverview.svelte
@@ -1,5 +1,8 @@
 <script lang="js">
   import { Content, Grid, Row, Column, Tile } from "carbon-components-svelte";
+  let totalCount = 600;
+  let validCount = 587;
+  let uniqueCount = 200;
 </script>
 
 <Content>
@@ -8,14 +11,14 @@
     <Grid>
       <Row>
         <Column>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
+          <Row><h4>{totalCount}</h4></Row>
+          <Row><h4>{validCount}</h4></Row>
+          <Row><h4>{uniqueCount}</h4></Row>
         </Column>
         <Column>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
-          <Row><h4></h4></Row>
+          <Row><h5>Total Nominations</h5></Row>
+          <Row><h5>Valid Nominations</h5></Row>
+          <Row><h5>Unique Nominations</h5></Row>
         </Column>
       </Row>
     </Grid>
