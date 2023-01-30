fd7b632a1e9afc5ae5ecb90bc73c52e5f5364345
Kelsey Donovan <kelseydonovan@Kelseys-MacBook-Pro.local>
2023-01-30 10:16:06 -0500

    hard coded count

--- dashboard/src/components/dashHome/NomineeCategories.svelte
+++ dashboard/src/components/dashHome/NomineeCategories.svelte
@@ -2,7 +2,7 @@
   import { Content, Tile, DataTable } from "carbon-components-svelte";
 
   // the header for the DataTable
-  let dataTableHeader = [{ key: "name", value: "Categories" }];
+  let dataTableHeader = [{ key: "name", value: "Categories" },{ key: "value", value: "Count" }];
 
   // TODO: replace once backend is setup
   let dataTableRows = [
@@ -10,40 +10,48 @@
       id: "1",
       name: "Art",
       subcategory: "Architecture, Art History, Cinema, Design, Literature, Music, Painting/Drawing, Sculpting, Theater/Dance, Other",
+      value: "20",
     },
     {
       id: "2",
       name: "Athletics",
       subcategory: "Competition Sports, individual Sports, Team Sports, Sports/Team Management, Other",
+      value: "20",
     },
     {
       id: "3",
       name: "Business",
       subcategory: "CEO",
+      value: "20",
     },
     {
       id: "5",
       name: "Eduction",
       subcategory: "Early Eduaction",
+      value: "20",
     },
     {
       id: "6",
       name: "Humanities",
       subcategory: "Ancient/Modern Languages",
+      value: "20",
     },
     {
       id: "7",
       name: "Public Service/Government",
       subcategory: "Activism",
+      value: "20",
     },
     {
       id: "8",
       name: "STEM",
       subcategory: "Astronomy",
+      value: "20",
     },
     {
       id: "9",
       name: "Other",
+      value: "20",
     },
   ];
 </script>
