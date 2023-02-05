21ed98c6fee88142fa2c0e2cd6eb92422522abaa
May wu <mw4815@rit.edu>
2023-02-05 11:59:57 -0500

    nominations page cleanup

--- dashboard/src/routes/nominations/+page.svelte
+++ dashboard/src/routes/nominations/+page.svelte
@@ -3,24 +3,30 @@
   import "../../css/index.css";
   import Navigation from "../../components/Navigation.svelte";
   import { Content, DataTable, OverflowMenu, OverflowMenuItem } from "carbon-components-svelte";
+  import NominationInformation from "../../components/nominations/NominationInformation.svelte";
 
   export let data;
   export let { nominations } = data.props;
 
-  let headers = [
-    { key: "name", value: "Name"},
-    { key: "category", value: "Category"},
-    { key: "overflow", empty: true},
-  ];
+  var headers = [
+      { key: "data.id", empty: true},
+      { key: "nominee", value: "Nominee" },
+      { key: "nominator", value: "Nominated By" },
+      { key: "date", value: "Date" },
+    ]
 
   var populateRows = (nominations) => {
     let rows = [];
     let rowID = 1;
     Object.entries(nominations).forEach(([key, nomination], index) => {
       let data = {
+        data: {
+          id: rowID
+        },
         id: rowID++,
-        name: nomination.nominee.firstName + " " + nomination.nominee.lastName,
-        category: nomination.nominee.category
+        nominee: nomination["nom-first"] + " " + nomination["nom-last"],
+        nominator: nomination["author-first"] + " " + nomination["author-last"],
+        date: nomination.date,
       }
       rows.push(data);
     });
@@ -33,21 +39,7 @@
 <main>
   <header><Navigation /></header>
   <Content>
-    <h1>Nominees</h1>
-    <DataTable zebra stickyHeader size="tall"
-    headers={headers} rows={rows}>
-      <svelte:fragment slot="cell" let:cell>
-        {#if cell.key === "overflow"}
-          <OverflowMenu flipped>
-            <OverflowMenuItem text="Restart" />
-            <OverflowMenuItem
-              href="https://cloud.ibm.com/docs/loadbalancer-service"
-              text="API documentation"
-            />
-            <OverflowMenuItem danger text="Stop" />
-          </OverflowMenu>
-        {:else}{cell.value}{/if}
-      </svelte:fragment>
-    </DataTable>
+    <h1>Nominations</h1>
+    <NominationInformation {rows}/>
   </Content>
 </main>
--- dashboard/src/routes/nominations/[id]/+page.svelte
+++ dashboard/src/routes/nominations/[id]/+page.svelte
@@ -2,43 +2,14 @@
   import "carbon-components-svelte/css/all.css";
   import "../../../css/index.css";
   import Navigation from "../../../components/Navigation.svelte";
-  import {
-    Content,
-    SideNavDivider,
-    Column,
-    Row,
-  } from "carbon-components-svelte";
+  import { Content } from "carbon-components-svelte";
   export let data;
   export let { n } = data.props;
 </script>
 
 <main>
   <header><Navigation /></header>
-  <body>
-    <Content style="padding-left: $spacing-05">
-      <Column>
-        <h1>Nomination</h1>
-
-        <h2>{n.nominee.firstName} {n.nominee.lastName}</h2>
-
-        <h3>
-          Nominated by {n.nominator.firstName}
-          {n.nominator.lastName} on {n.date}.
-        </h3>
-
-        <p>Nomination Category: <b>{n.nominee.category}</b></p>
-
-        <p>
-          This nominee is{n.nominee.deceased ? " " : " not "}dead.
-        </p>
-
-        <form method="POST" action="?/createNomination">
-          <input type="text" name="title" placeholder="Title" />
-          <input type="text" name="body" placeholder="Body" />
-          <input type="number" name="userId" value="6" />
-          <input type="submit" value="Create!" />
-        </form>
-      </Column>
-    </Content>
-  </body>
+  <Content>
+    <h1>Nomination</h1>
+  </Content>
 </main>
