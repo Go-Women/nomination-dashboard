fbdcdc95c5f914d0afd8ddcb78aa146625c5f736
May wu <mw4815@rit.edu>
2023-02-28 17:04:42 -0500

    show matches after button is clicked

--- dashboard/src/routes/matching/+page.svelte
+++ dashboard/src/routes/matching/+page.svelte
@@ -6,10 +6,16 @@
     Grid,
     Button,
     InlineNotification,
+    DataTable
   } from "carbon-components-svelte";
   import "carbon-components-svelte/css/all.css";
   import "../../css/index.css";
   import Navigation from "../../components/Navigation.svelte";
+
+  let buttonClicked = false;
+  function showMatches() {
+    buttonClicked = true;
+  }
 </script>
 
 <main>
@@ -22,13 +28,28 @@
       </Breadcrumb>
 
       <h1>Matching</h1>
-      <InlineNotification
-        lowContrast
-        kind="error"
-        subtitle="There aren't any potential matches yet. Click below to generate new matches."
-      />
-      <Button iconDescription="View" href={"/home"}>Generate New Matches</Button
-      >
+      {#if buttonClicked === false}
+        <InlineNotification
+          lowContrast
+          kind="error"
+          subtitle="There aren't any potential matches yet. Click below to generate new matches."
+        />
+      {/if}
+      <Button iconDescription="View" on:click|once={showMatches}>Generate New Matches</Button>
+
+      {#if buttonClicked === true}
+        <DataTable
+            headers={[
+              { key: "nomineeName", value: "Nominee Name" },
+              { key: "nomineeCategory", value: "Nominee Category" },
+              { key: "judgeName", value: "Judge Name" },
+              { key: "judgeCategory", value: "Judge Category" },
+              { key: "judgeCapacity", value: "Judge Capacity" },
+              { key: "action", value: "Action" },
+            ]}
+            rows={[]}
+          />
+      {/if}
     </Grid>
   </Content>
 </main>
