a720c98d9683a97a9986f7f0e2fcfa327cf35995
Celeste Gambardella <cg7346@rit.edu>
2023-02-28 18:23:37 -0500

    matching input example

--- backend/src/matching.js
+++ backend/src/matching.js
@@ -2,6 +2,20 @@ const judges = [];
 const nominees = [];
 const matches = {};
 
+// Dataset:
+// {
+//   'nomineeID':'stringID' ,
+//   'nomCategory': 'string',
+//   'nomSubCategory': 'string',
+//   'nomSubcatgoryOther:' 'string',
+//   'judgeID':'stringID' ,
+//   'judgeCategory': 'string',
+//   'judgeSubcategory': 'string',
+//   'judgeSubcategoryOther:' 'string'
+//   'judgeCapacity': 'int'
+// }
+
+
 function getJudgeSubCat(judge){
     var Subcat = "Math";
     return Subcat;
