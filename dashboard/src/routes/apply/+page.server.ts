31ff74d749e7da352b9157a3e7887df607e4d0bc
Giovanni Melchionne <gtm7712@rit.edu>
2023-03-04 14:56:44 -0500

    Judge application form

---
+++ dashboard/src/routes/apply/+page.server.ts
@@ -0,0 +1,22 @@
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+  
+  default: async ({request}) => {
+    const formData = await request.formData();
+    const data: { [name: string]: any } = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+    const res = await fetch('http://localhost:8000/nominations', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+  } 
+};
