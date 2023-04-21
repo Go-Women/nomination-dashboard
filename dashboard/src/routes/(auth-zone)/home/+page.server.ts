57273c8b2696b8bd30577498d662fff0eea4790a
Giovanni Melchionne <gtm7712@rit.edu>
2023-04-21 11:03:44 -0400

    Block judges with status !== j300 from home page

--- dashboard/src/routes/(auth-zone)/home/+page.server.ts
+++ dashboard/src/routes/(auth-zone)/home/+page.server.ts
@@ -11,7 +11,17 @@ if (dev) {
   FUNCTIONS_KEY = `${process.env.DEFAULTKEY}`;
 }
 
-export const load: PageServerLoad = async ({fetch}) => {
+export const load: PageServerLoad = async ({fetch, cookies}) => {
+  const user = cookies.get('user');
+  if (!user) throw error(401, "Unauthorized.");
+  const authRes = await fetch(`https://nwhofapi.azurewebsites.net/api/getuserbyfirebaseid/${user}`,{headers:{'x-functions-key':FUNCTIONS_KEY}});
+  if (authRes.ok) {
+    let authInfo = (await authRes.json())[0];
+    if (authInfo['type'] === 'judge' && JSON.parse(authInfo['info'])['judgeStatus'] !== 'j300') throw error(401, "Unauthorized.");
+  } else {
+    throw error(authRes.status, 'An error occured while fetching data for this page.');
+  }
+
   const res = await fetch(`https://nwhofapi.azurewebsites.net/api/nominations`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
   if (res.ok) {
     const nominations = await res.json();
