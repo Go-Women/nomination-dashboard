a461fd93478c78595ed41dd07803f41bbd0ab33f
Celeste Gambardella <cg7346@rit.edu>
2023-04-23 00:12:35 -0400

    judge home fix

--- dashboard/src/routes/(auth-zone)/home/+page.server.ts
+++ dashboard/src/routes/(auth-zone)/home/+page.server.ts
@@ -14,27 +14,35 @@ if (dev) {
 export const load: PageServerLoad = async ({fetch, cookies}) => {
   const user = cookies.get('user');
   let authInfo;
+  let jMatches;
   if (!user) throw error(401, "Unauthorized.");
   const authRes = await fetch(`https://nwhofapi.azurewebsites.net/api/getuserbyfirebaseid/${user}`,{headers:{'x-functions-key':FUNCTIONS_KEY}});
   if (authRes.ok) {
     authInfo = (await authRes.json())[0];
-    if (authInfo['type'] === 'judge' && JSON.parse(authInfo['info'])['judgeStatus'] !== 'j300') throw error(401, "Unauthorized.");
+    if (authInfo['type'] === 'judge' && JSON.parse(authInfo['info'])['judgeStatus'] !== 'j300')
+      throw error(401, "Unauthorized.");
+    if (authInfo['type'] === 'judge') {
+      const res5 = await fetch(`https://nwhofapi.azurewebsites.net/api/judges/${authInfo["ID"]}/matches`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
+      if (res5.ok) {
+        jMatches = await res5.json();
+      } else {
+        throw error(res5.status, 'An error occurred while fetching data for this page.');
+      } 
+    }
   } else {
-    throw error(authRes.status, 'An error occured while fetching data for this page.');
+    throw error(authRes.status, 'An error occurred while fetching data for this page.');
   }
 
   const res1 = await fetch(`https://nwhofapi.azurewebsites.net/api/nominations`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
   const res2 = await fetch(`https://nwhofapi.azurewebsites.net/api/nominees`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
   const res3 = await fetch(`https://nwhofapi.azurewebsites.net/api/judges`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
   const res4 = await fetch(`https://nwhofapi.azurewebsites.net/api/matches`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
-  const res5 = await fetch(`https://nwhofapi.azurewebsites.net/api/judges/${authInfo["ID"]}/matches`, {headers:{'x-functions-key':FUNCTIONS_KEY}});
 
   if (res1.ok && res2.ok && res3.ok && res4.ok) {
     const nominations = await res1.json();
     const nominees = await res2.json();
     const judges = await res3.json();
     const matches = await res4.json();
-    const jMatches = await res5.json();
 
     return {
       props: {
@@ -47,6 +55,9 @@ export const load: PageServerLoad = async ({fetch, cookies}) => {
       }
     };
   } else {
-    throw error(res.status, 'An error occurred while fetching data for this page.')
+    if (!res1.ok) throw error(res1.status, 'An error occurred while fetching data for this page.');
+    if (!res2.ok) throw error(res2.status, 'An error occurred while fetching data for this page.');
+    if (!res3.ok) throw error(res3.status, 'An error occurred while fetching data for this page.');
+    if (!res4.ok) throw error(res4.status, 'An error occurred while fetching data for this page.');
   }
 }
